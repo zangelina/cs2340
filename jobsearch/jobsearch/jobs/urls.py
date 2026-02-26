a31c016981461feb926d48f8cd42978a17b98520
Levian-Liu-Guanyi <zhiweisdsz@hotmail.com>
2026-02-26 16:25:51 +0800

    Update urls.py for User story 16

--- jobsearch/jobsearch/jobs/urls.py
+++ jobsearch/jobsearch/jobs/urls.py
@@ -46,4 +46,5 @@ urlpatterns = [
     # Map + Recommendations
     path("map/", views.job_map, name="job_map"),
     path("recommended/", views.recommended_jobs, name="recommended_jobs"),
+    path("recruiter/recommended/", views.recommended_candidates, name="recommended_candidates"),
 ]
