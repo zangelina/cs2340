08e26031880f5fa4cd979e2d3363954f99b970e0
Levian-Liu-Guanyi <zhiweisdsz@hotmail.com>
2026-03-13 06:08:29 +0800

    fix one mistake

--- jobsearch/jobsearch/jobs/admin.py
+++ jobsearch/jobsearch/jobs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.http import HttpResponse import csv
+from django.http import HttpResponse 
+import csv
 from .models import CustomUser, JobPosting
 
 #Neal
